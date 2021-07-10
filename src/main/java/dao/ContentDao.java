90a345d5638b9ffec9c34fe48ce8a48a4089bec5
robinjoon <45223837+robinjoon@users.noreply.github.com>
2021-07-11 00:57:38 +0900

    Create dao package and classes

---
+++ src/main/java/dao/ContentDao.java
@@ -0,0 +1,10 @@
+package dao;
+import dto.*;
+public class ContentDao {
+	public boolean insert(Content content) {
+		return true;
+	}
+	public Content get(String device, String album, int photoId) {
+		return null;
+	}
+}

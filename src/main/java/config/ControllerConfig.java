e3e6d7bdee8aec28d0f679b8407cbadbf92a28eb
robinjoon <45223837+robinjoon@users.noreply.github.com>
2021-07-11 00:19:28 +0900

    First Commit

---
+++ src/main/java/config/ControllerConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@Configuration
+public class ControllerConfig {
+/*
+	@Bean
+	public AccountBookController accountBookController() {
+		return new AccountBookController();
+	}
+*/
+}
---
+++ src/main/java/config/DaoConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DaoConfig {
+	
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		DataSource ds = new DataSource();
+		return ds;
+	}
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
+	}
+}

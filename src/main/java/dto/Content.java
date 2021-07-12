8e4c9ae60faeba22ca6c8db832ad34f763f2bfa5
robinjoon <45223837+robinjoon@users.noreply.github.com>
2021-07-12 23:11:32 +0900

    update dto.Content

--- src/main/java/dto/Content.java
+++ src/main/java/dto/Content.java
@@ -3,6 +3,7 @@ package dto;
 public class Content {
 	String device;
 	String album;
+	int contentId;
 	String originalFileName;
 	String systemFileName;
 	public Content(String device, String album, String originalFileName, String systemFileName) {
@@ -24,5 +25,13 @@ public class Content {
 	public String getSystemFileName() {
 		return systemFileName;
 	}
+	public int getContentId() {
+		return contentId;
+	}
+	public void setContentId(int contentId) {
+		this.contentId = contentId;
+	}
+	
+	
 	
 }
